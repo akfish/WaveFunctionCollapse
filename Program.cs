14ec886d3c8470cfe63b7e643ed00c1a03bcf809
AKFish <akfish@gmail.com>
2023-05-27 23:15:46 +0800

    Improve console output

--- Program.cs
+++ Program.cs
@@ -17,7 +17,6 @@ static class Program
     XmlSerializer serializer = new XmlSerializer(typeof(Samples));
     Samples samples = (Samples)serializer.Deserialize(System.IO.File.OpenRead("samples.xml"));
 
-    Random random = new();
 
     SampleBase[] samplesToRun = names.Length == 0
         ? samples.Items
@@ -29,33 +28,49 @@ static class Program
       return;
     }
 
+    Console.WriteLine($"Found {samplesToRun.Length} / {samples.Items.Length} samples in {sw.ElapsedMilliseconds} ms");
+
     // For each sample in the XML file
     foreach (SampleBase sample in samplesToRun)
     {
       Model model;
-      Console.WriteLine($"< {sample.name}");
+      Console.WriteLine($"==========================================================");
+      Console.WriteLine($"Sample:\t{sample.name}");
+      Console.Write("Type:\t");
+      sw.Restart();
       if (sample is Overlapping)
       {
         Overlapping o = (Overlapping)sample;
         model = new OverlappingModel(o.name, o.size, o.size, o.periodicInput, o.periodic, o.symmetry, o.ground, o.heuristic);
+        Console.WriteLine("Overlapping");
       }
       else if (sample is SimpleTiled)
       {
         SimpleTiled s = (SimpleTiled)sample;
         model = new SimpleTiledModel(s.name, s.subset, s.size, s.size, s.periodic, s.blackBackground, s.heuristic);
+        Console.WriteLine("SimpleTiled");
       }
       else
       {
-        Console.WriteLine("Unknown sample type");
+        Console.WriteLine("Unknown (Skipping)");
         continue;
       }
+      Console.WriteLine($"> Created in {sw.ElapsedMilliseconds}ms");
+      // Use the same seed for each run
+      Random random = new(42);
       for (int i = 0; i < sample.screenshots; i++)
       {
+        // Update the seed for each screenshot
+        int seed = random.Next();
         for (int k = 0; k < 10; k++)
         {
-          Console.Write("> ");
-          int seed = random.Next();
+          Console.Write($"> Run {i + 1}/{sample.screenshots}\t{k + 1}/10\t\t");
+          sw.Restart();
           bool success = model.Run(seed, sample.limit);
+
+          Console.Write($"<{seed}>\t");
+          Console.Write($"{sw.ElapsedMilliseconds}ms\t");
+
           if (success)
           {
             Console.WriteLine("DONE");
@@ -64,11 +79,15 @@ static class Program
               System.IO.File.WriteAllText($"output/{sample.name} {seed}.txt", stmodel.TextOutput());
             break;
           }
-          else Console.WriteLine("CONTRADICTION");
+          else
+          {
+            Console.WriteLine("CONTRADICTION");
+            // If we fail, try again with a new seed
+            seed = random.Next();
+          }
         }
       }
     }
 
-    Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
   }
 }
